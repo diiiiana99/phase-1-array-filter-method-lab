c02fcb64c8fc755608c1e04ffdf2a25353007a43
diiiiana99 <93669061+diiiiana99@users.noreply.github.com>
2022-01-12 17:26:05 -0500

    commit tree 32ca91563a770e98837e68a5cda9a31b13cbcb50

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching (drivers, name) {
+    return drivers.filter(driver => {if (driver.toLowerCase() === name.toLowerCase()){
+    return driver;
+    }})
+}
+function fuzzyMatch (drivers, letters) {
+    return drivers.filter(driver => {if (driver.indexOf(letters) === 0){
+        return driver;
+    }})
+}
+function matchName(drivers, string) {
+    return drivers.filter (driver => driver.name === string)
+}
